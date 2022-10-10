9aeaa75e297352b4bd9160c54606e4060dbde0b2
morival <64006835+morival@users.noreply.github.com>
2022-10-10 23:04:32 +0100

    managed white space in testimonials

--- src/elements/HomeTestimonials.js
+++ src/elements/HomeTestimonials.js
@@ -1,26 +1,30 @@
 import { Carousel } from "react-bootstrap";
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const TestimonialsSection = styled.section`
-    background-color: lightgray;    
+    padding-block: 40px;
+    background-color: lightgray;
 `;
 export const TestimonialsCarousel = styled(Carousel)`
-    padding: 40px 0;
+    /* padding-block: 20px; */
 `;
 // export const TestimonialsCarouselBackground = styled.img`
 //     display: block;
 //     width: 100%;
 // `;
 export const TestimonialsCarouselCaption = styled(Carousel.Caption)`
+    ${media.greaterThan('medium')`
+        max-width: 600px;
+        border-radius: 10px;
+    `}
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
     position: static;
-    max-width: 600px;
     padding-inline: 20px;
     font-style: italic;
     background-color: #b7b7b7;
-    border-radius: 10px;
 `;
 export const TestimonialsCarouselItem = styled(Carousel.Item)`
     
@@ -31,14 +35,18 @@ export const TestimonialsCarouselName = styled.p`
     padding-inline-end: 50px;
 `;
 export const TestimonialsCarouselText = styled.div`
-    min-height: 330px;
+    min-height: 400px;
     display: flex;
     justify-content: center;
     position: relative;
-    bottom: 1.25rem;
-    padding-top: 1.25rem;
-    padding-bottom: 1.25rem;
+    /* bottom: 1.25rem; */
+    padding-block: 50px;
 `;
 export const TestimonialsComment = styled.p`
-    
+    ${media.lessThan('medium')`
+        font-size: 0.95rem;
+    `}
+    ${media.lessThan('small')`
+        font-size: 0.9rem;
+    `}
 `;
--- src/projects/balerno.md
+++ src/projects/balerno.md
@@ -1,9 +1,9 @@
 ---
-title: Balerno (exptensions and renovation)
+title: Balerno (extensions and renovation)
 location: Balerno
 services: extensions, new builds
 slug: balerno
 thumb: balerno-thumb
 ---
 
-**Heading 2 for the Balerno (exptensions and renovation) project** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+**Heading 2 for the Balerno (extensions and renovation) project** Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
